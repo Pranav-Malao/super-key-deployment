c6aa94225f4cb38cf9ec578f1cf8e7edd605f596
Pranav-Malao <pranavmalao909@gmail.com>
2025-08-20 15:46:30 +0530

    removed mongodb

--- super_key/src/app.js
+++ super_key/src/app.js
@@ -26,11 +26,15 @@ app.use((req, res, next) => {
 app.use(cookieParser());
 console.log('process.env.MONGODB_URI:', process.env.MONGODB_URI);
 // MongoDB session store
-const store = new MongoDBStore({
-  uri: "mongodb+srv://harsh:Asdf@1234@cluster0.hsnb1na.mongodb.net/super_key_sessions?retryWrites=true&w=majority&appName=Cluster0",
-  collection: 'sessions'
-});
-
+// const store = new MongoDBStore({
+//   uri: 'mongodb+srv://harsh:Asdf%401234@cluster0.hsnb1na.mongodb.net/super_key_sessions?retryWrites=true&w=majority&appName=Cluster0',
+//   collection: 'sessions'
+// });
+app.use(session({
+  secret: 'test',
+  resave: false,
+  saveUninitialized: true,
+}));
 store.on('error', (error) => {
   console.error('MongoDB Session Store Error:', error);
 });
